28542b267d5d1beacf58b19e1148a7673e0ab292
Javi3r1002 <65726905+Javi3r1002@users.noreply.github.com>
2022-05-03 23:50:01 -0600

    Update HT7_SVM.py

--- HT7_SVM.py
+++ HT7_SVM.py
@@ -147,3 +147,175 @@ print('Hopkins', pyclustertend.hopkins(X,len(X)))
 
 print(X.shape)
 X_scale=sklearn.preprocessing.scale(X)
+
+
+
+pyclustertend.vat(X_scale)
+pyclustertend.vat(X)
+plt.show()
+"""
+"""
+numeroClusters = range(1,11)
+wcss = []
+for i in numeroClusters:
+    kmeans = cluster.KMeans(n_clusters=i)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+
+plt.plot(numeroClusters, wcss)
+plt.xlabel("Número de clusters")
+plt.ylabel("Puntuación")
+plt.title("Gráfico de Codo")
+
+
+"""
+
+"""
+rango_n_clusters = [2, 3, 4, 5]
+
+for n_clusters in rango_n_clusters:
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    fig.set_size_inches(18, 7)
+    ax1.set_xlim([-0.1, 1])
+    ax1.set_ylim([0, len(X) + (n_clusters + 1) * 10])
+    clusterer = KMeans(n_clusters=n_clusters, random_state=10)
+    cluster_labels = clusterer.fit_predict(X)
+    silhouette_avg = silhouette_score(X, cluster_labels)
+    print(
+        "For n_clusters =",
+        n_clusters,
+        "The average silhouette_score is :",
+        silhouette_avg,
+    )
+
+    sample_silhouette_values = silhouette_samples(X, cluster_labels)
+
+    y_lower = 10
+    for i in range(n_clusters):
+        ith_cluster_silhouette_values = sample_silhouette_values[cluster_labels == i]
+        ith_cluster_silhouette_values.sort()
+        size_cluster_i = ith_cluster_silhouette_values.shape[0]
+        y_upper = y_lower + size_cluster_i
+        color = cm.nipy_spectral(float(i) / n_clusters)
+        ax1.fill_betweenx(
+            np.arange(y_lower, y_upper),
+            0,
+            ith_cluster_silhouette_values,
+            facecolor=color,
+            edgecolor=color,
+            alpha=0.7,
+        )
+        ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
+        y_lower = y_upper + 10  # 10 for the 0 samples
+
+    ax1.set_title("The silhouette plot for the various clusters.")
+    ax1.set_xlabel("The silhouette coefficient values")
+    ax1.set_ylabel("Cluster label")
+    ax1.axvline(x=silhouette_avg, color="red", linestyle="--")
+    ax1.set_yticks([])  # Clear the yaxis labels / ticks
+    ax1.set_xticks([-0.1, 0, 0.2, 0.4, 0.6, 0.8, 1])
+    colors = cm.nipy_spectral(cluster_labels.astype(float) / n_clusters)
+    ax2.scatter(
+        X[:, -1], X[:, 0], marker=".", s=30, lw=0, alpha=0.7, c=colors, edgecolor="k"
+    )
+    centers = clusterer.cluster_centers_
+    ax2.scatter(
+        centers[:, -1],
+        centers[:, 0],
+        marker="o",
+        c="white",
+        alpha=1,
+        s=200,
+        edgecolor="k",
+    )
+
+    for i, c in enumerate(centers):
+        ax2.scatter(c[-1], c[0], marker="$%d$" % i, alpha=1, s=50, edgecolor="k")
+
+    ax2.set_title("The visualization of the clustered data.")
+    ax2.set_xlabel("Feature space for the 1st feature")
+    ax2.set_ylabel("Feature space for the 2nd feature")
+
+    plt.suptitle(
+        "Silhouette analysis for KMeans clustering on sample data with n_clusters = %d"
+        % n_clusters,
+        fontsize=14,
+        fontweight="bold",
+    )
+"""
+
+km = cluster.KMeans(n_clusters=3, random_state = 5).fit(X)
+centroides = km.cluster_centers_
+#print(centroides)
+
+
+normal = km.predict(X)
+#plt.scatter(X[normal == 0, -1], X[normal == 0, -1],s=100,c='red', label = "Cluster 1")
+Cluster_bajo = X[normal == 0, -1]
+Cluster_bajo = Cluster_bajo.tolist()
+print('máximo primer cluster (rojo)', max(Cluster_bajo))
+print('mínimo primer cluster (rojo)',min(Cluster_bajo))
+Barato = H[((H['SalePrice']>(min(Cluster_bajo)))& (H['SalePrice']<max(Cluster_bajo)))]
+
+#plt.scatter(X[normal == 1, -1], X[normal == 1, -1],s=100,c='blue', label = "Cluster 2")
+Cluster_medio = X[normal == 1, -1]
+Cluster_medio = Cluster_medio.tolist()
+print('máximo segundo cluster (azul)', max(Cluster_medio))
+print('mínimo segundo cluster (azul)',min(Cluster_medio))
+Medio = H[((H['SalePrice']>(min(Cluster_medio))) & (H['SalePrice']<max(Cluster_medio)))]
+
+#plt.scatter(X[normal == 2, -1], X[normal == 2, -1],s=100,c='green', label = "Cluster 3")
+Cluster_alto = X[normal == 2, -1]
+Cluster_alto = Cluster_alto.tolist()
+print('máximo tercer cluster (verde)', max(Cluster_alto))
+print('mínimo tercer cluster (verde)',min(Cluster_alto))
+Alto =  H[((H['SalePrice']>(min(Cluster_alto))) & (H['SalePrice']<max(Cluster_alto)))]
+
+#plt.scatter(km.cluster_centers_[:,-1],km.cluster_centers_[:,-1], s=300, c="yellow",marker="*", label="Centroides")
+#plt.title("Grupo casa")
+#plt.xlabel("Precio de venta")
+#plt.ylabel("Precio de venta")
+#plt.legend()
+
+
+#plt.show()
+
+
+
+CH = H.columns.values
+
+Cate = []
+for row in H['SalePrice']:
+    if row in Cluster_bajo : Cate.append(1)
+    elif row in Cluster_medio:   Cate.append(2)
+    elif row in Cluster_alto:  Cate.append(3)
+
+    
+    else:
+        print('dndofinoid')
+H['Categoria'] = Cate
+print(H.groupby('Categoria').size())
+G = H
+H = H.drop(['SalePrice'], axis = 1)
+#sns.pairplot(H, hue="Categoria",  diag_kws={'bw': 100})
+#plt.tight_layout()
+#plt.show()
+"""
+print(Barato.describe().transpose())
+print(Medio.describe().transpose())
+print(Alto.describe().transpose())
+"""
+#Modelo para casas baratas
+
+H = G
+Cate = []
+for row in H['SalePrice']:
+    if row in Cluster_bajo : Cate.append(0)
+    elif row in Cluster_medio:   Cate.append(1)
+    elif row in Cluster_alto:  Cate.append(2)
+
+    
+    else:
+    	print('dndofinoid')
+H['Categoria'] = Cate
+H = H.drop(['SalePrice'], axis = 1)
